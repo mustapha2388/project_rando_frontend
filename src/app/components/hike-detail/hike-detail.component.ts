dddec10f1bfc9653d56485001c0053d861fe2c05
mouss <mouss2388@gmail.com>
2020-01-04 18:05:19 +0100

    Get hike data from database

--- src/app/components/hike-detail/hike-detail.component.ts
+++ src/app/components/hike-detail/hike-detail.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {HikeRepositoryService} from '../../services/hike-repository.service';
+import {error} from 'util';
+import {Hike} from '../../models/hike';
 
 @Component({
   selector: 'app-hike-detail',
@@ -8,6 +10,7 @@ import {HikeRepositoryService} from '../../services/hike-repository.service';
   styleUrls: ['./hike-detail.component.css']
 })
 export class HikeDetailComponent implements OnInit {
+  hike: Hike;
   constructor(private hikeRepositoryService: HikeRepositoryService,
               private router: Router,
               private activatedRoute: ActivatedRoute ) { }
@@ -15,10 +18,12 @@ export class HikeDetailComponent implements OnInit {
   ngOnInit() {
     const id: number = +this.activatedRoute.snapshot.params.id;
     this.hikeRepositoryService.getHikeById(id)
-      .then(h => console.log('find it ' + h.description));
+      .then(h => this.hike = h)
+      .catch(er => this.router.navigate(['']))
+      .finally(() => { console.log('hike-detail is loaded'); });
   }
 
   onBack() {
-    this.router.navigate(['']);
+    this.router.navigate(['']).then();
   }
 }
