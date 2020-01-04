25e44856620874ff496326abf931fd17cb97fa8f
mouss <mouss2388@gmail.com>
2020-01-04 15:01:39 +0100

    Change page with routerlink
    - add routerlink
    - use navigate method
    - create hike detail component

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
 import {HikeListeComponent} from '../components/hike-liste/hike-liste.component';
+import {HikeDetailComponent} from '../components/hike-detail/hike-detail.component';
 
 
 export const ROUTES: Routes = [
+  {path: '', redirectTo: 'hikes', pathMatch: 'full'},
   {path: 'hikes', component: HikeListeComponent},
-  {path: '', redirectTo: 'hikes', pathMatch: 'full'}
+  {path: 'hike-detail', component: HikeDetailComponent}
 ];
 
 @NgModule({
